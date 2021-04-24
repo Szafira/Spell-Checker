a2ceafc760f864705edecd17de66a0f187928406
Klaudia Sawicka <sawicka.klaudi@gmail.com>
2021-04-24 10:50:01 +0200

    Creating Async class

---
+++ app/src/main/java/com/dnd/spellchecker/Async.java
@@ -0,0 +1,48 @@
+package com.dnd.spellchecker;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class Async extends AsyncTask<Void, Void, String> {
+    public String jsonToString;
+    JSONObject jsonRead = new JSONObject();
+
+
+    @Override
+    public String doInBackground(Void... params) {
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL("https://www.dnd5eapi.co/api/spells");
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            int code = urlConnection.getResponseCode();
+            if (code != 200) {
+                throw new IOException("Invalid response from server: " + code);
+            }
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    urlConnection.getInputStream()));
+            String line;
+
+            while ((line = rd.readLine()) != null) {
+                jsonRead.put("Spell", line);
+
+            }
+            jsonToString = jsonRead.toString();
+            System.out.println(jsonToString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return jsonToString;
+    }
+}
--- app/src/main/java/com/dnd/spellchecker/MainActivity.java
+++ app/src/main/java/com/dnd/spellchecker/MainActivity.java
@@ -1,10 +1,8 @@
 package com.dnd.spellchecker;
 
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,11 +13,7 @@ import com.dnd.spellchecker.databinding.ActivityMainBinding;
 
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements DialogAnswerResult {
 
@@ -29,42 +23,6 @@ public class MainActivity extends AppCompatActivity implements DialogAnswerResul
     String jsonToString = new String();
     HttpURLConnection urlConnection = null;
 
-
-
-    class Async extends AsyncTask<Void, Void, String> {
-        public String jsonToString;
-
-        @Override
-        protected String doInBackground(Void... params) {
-            HttpURLConnection urlConnection = null;
-
-            try {
-                URL url = new URL("https://www.dnd5eapi.co/api/spells");
-                urlConnection = (HttpURLConnection) url.openConnection();
-
-                int code = urlConnection.getResponseCode();
-                if (code != 200) {
-                    throw new IOException("Invalid response from server: " + code);
-                }
-
-                BufferedReader rd = new BufferedReader(new InputStreamReader(
-                        urlConnection.getInputStream()));
-                String line;
-
-                while ((line = rd.readLine()) != null) {
-                    jsonRead.put("Spell", line);
-
-                }
-                jsonToString = jsonRead.toString();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-                return jsonToString;
-            }
-
-    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,17 +30,23 @@ public class MainActivity extends AppCompatActivity implements DialogAnswerResul
         setLabels();
         setStartUI();
         setContentView(binding.getRoot());
-        Button buttonSearch = findViewById(R.id.buttonSearch);
 
+    }
 
+    public void search(View view) {
+
+        TextView textView = findViewById(R.id.result_rv);
         String BaseUrl = "https://www.dnd5eapi.co/api/";
         String CategoryUrl = getString(R.string.Spells);
         String fullUrl = BaseUrl + CategoryUrl + "/paladin";
 
+        Async async = new Async();
+        async.execute();
 
-
+        String results = "Test :v";
+        System.out.println(jsonToString);
+        System.out.println(results);
     }
-
 
     private void setStartUI() {
         setListenerForOptionOne();
@@ -149,21 +113,6 @@ public class MainActivity extends AppCompatActivity implements DialogAnswerResul
         }
         currentDialog.dismiss();
     }
-
-    public void search(View view) {
-        TextView textView = findViewById(R.id.result_rv);
-
-        Async async = new Async();
-        async.execute();
-
-        String results = "Test :v";
-        textView.setText(jsonToString);
-        System.out.println(jsonToString);
-        System.out.println(results);
-
-
-    }
-
 }
 
 
